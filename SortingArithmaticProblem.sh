d2be9a8c664e2cc7bab30843b9fbed1eba5d863f
rajchaudhari09 <rajchaudhari366@gmail.com>
2020-03-18 17:27:54 +0530

    ADDED: Take a three inputs a,b,c

---
+++ SortingArithmaticProblem.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -x
+#Arithmetic Computation & Sorting
+#This problem computes different arithmetic expressions and Sorts the results
+#Take The three inputs a, b & c
+read -p "Enter The First Number: " a
+read -p "Enter The Second Number: " b
+read -p "Enter The Third Number: " c
